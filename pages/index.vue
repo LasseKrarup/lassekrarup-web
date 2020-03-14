f399b80b54809d998f2501f100265468fca8a283
Lasse Herold Krarup <latze95@gmail.com>
2020-03-14 18:14:26 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="hero is-primary is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title">
+          {{ title }}
+        </h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+export default {
+  data () {
+    return ({
+      title: 'Drummer, musician, web designer'
+    })
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
