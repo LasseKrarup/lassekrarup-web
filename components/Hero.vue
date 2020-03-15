0a46a1f8abe31e52e88d98bd44229719c89af15b
Lasse Herold Krarup <latze95@gmail.com>
2020-03-15 15:54:07 +0100

    Turned Hero into a component

---
+++ components/Hero.vue
@@ -0,0 +1,66 @@
+<template>
+  <section class="hero">
+    <div class="hero-body">
+      <div class="container animated" :class="{ 'loaded': isLoaded }">
+        <div>
+          <slot name="first" />
+        </div>
+        <div class="animated content" :class="{ 'loaded': isLoaded }">
+          <slot name="second" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+  },
+  data () {
+    return ({
+      isLoaded: false
+    })
+  },
+  mounted () {
+    this.isLoaded = true
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.hero-body .container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transform: translateY(4rem);
+
+  &.loaded {
+    transform: translateY(0);
+  }
+
+  img {
+    margin: auto;
+
+  }
+}
+
+div.content {
+  &.animated {
+    opacity: 0;
+  }
+  width: 60vw;
+  text-align: justify;
+  padding-top: 2rem;
+
+  &.loaded {
+    opacity: 1;
+  }
+}
+
+.animated {
+  transition: transform 1.5s ease-in-out 0.5s, opacity 0.5s linear 2s;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,44 @@
 <template>
-  <section class="hero is-primary is-fullheight-with-navbar">
-    <div class="hero-body">
-      <div class="container is-vcentered is-flex">
-        <img src="~/assets/img/svg/LK_logo_white_text.svg" width="800px">
-      </div>
-    </div>
-  </section>
+  <Hero class="is-fullheight-with-navbar is-primary">
+    <template #first>
+      <img src="~/assets/img/svg/LK_logo_white_text.svg" width="800px">
+    </template>
+
+    <template #second>
+      <h1 class="title">
+        {{ title }}
+      </h1>
+
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. A impedit, voluptates provident ad eveniet rerum recusandae ducimus porro, quibusdam nesciunt nam quam. Aliquam itaque vel nobis quaerat blanditiis impedit quis dolore voluptas quas numquam iusto iure dolores, aspernatur, commodi explicabo. Dolorem sint impedit laudantium optio labore sit numquam corporis ducimus?
+      </p>
+    </template>
+  </Hero>
 </template>
 
 <script lang="ts">
-export default {
+import Vue from 'vue'
+import Hero from '~/components/Hero.vue'
+
+export default Vue.extend({
+
+  components: {
+    Hero
+  },
   data () {
     return ({
       title: 'Drummer, musician, web designer'
     })
+  },
+  head () {
+    const title:string = this.title
+    return ({
+      titleTemplate: 'Lasse Krarup - %s',
+      title
+    })
   }
-}
+})
 </script>
 
 <style lang="scss" scoped>
-.hero-body .container img{
-  margin: auto;
-}
 </style>
