5d192b96c1569eae581a67063b79e6e9ea49f5df
Lasse Herold Krarup <latze95@gmail.com>
2020-03-16 21:00:05 +0100

    Detachable, sticky navbar v1

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="navbar is-primary" role="navigation" aria-label="main navigation">
+  <nav class="navbar is-primary is-fixed-top" role="navigation" aria-label="main navigation" :class="{ 'has-scrolled': hasScrolled }">
     <div class="navbar-brand">
       <nuxt-link class="navbar-item" to="/">
-        <img src="~/assets/img/svg/LK_logo_white.svg" height="3rem" alt="Logo">
+        <img class="logo" src="~/assets/img/svg/LK_logo_white.svg" :class="{ 'has-scrolled': hasScrolled }" alt="Logo">
       </nuxt-link>
       <a
         role="button"
@@ -29,7 +29,9 @@
 </template>
 
 <script lang="ts">
-export default {
+import Vue from 'vue'
+
+export default Vue.extend({
   props: {
     links: {
       type: Array,
@@ -38,22 +40,62 @@ export default {
   },
   data () {
     return ({
-      isActive: false
+      isActive: false,
+      hasScrolled: false
     })
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  destroyed () {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll (event: Event) : void {
+      if (window.pageYOffset >= 50) {
+        this.hasScrolled = true
+      } else {
+        this.hasScrolled = false
+      }
+    }
   }
-}
+})
 </script>
 
 <style lang="scss" scoped>
+$light-background: lighten($primary, 5%);
+
 .navbar {
   box-sizing: border-box !important;
   height: 4rem;
   border-bottom: 1px solid rgba(255,255,255,0.3);
-}
 
-.navbar-item {
-  img {
+  transition-property: all, transform;
+  transition-duration: 0.5s;
+  transition-timing-function: ease-in-out;
+
+  .logo {
+    transition: all 0.5s ease-in-out;
     max-height: 3rem;
+    height: 3rem;
+
+    @media screen and (min-width: $desktop){
+      &.has-scrolled {
+        height: 4rem;
+        max-height: 4rem;
+      }
+    }
+  }
+
+  &.has-scrolled {
+    background-color: $light-background;
+    border-color: $light-background;
+    box-shadow: 0 0 5px rgba(0,0,0,0.5);
+
+    @media screen and (min-width: $desktop) {
+      height: 5rem;
+      font-size: 1.2rem;
+    }
   }
 }
 </style>
