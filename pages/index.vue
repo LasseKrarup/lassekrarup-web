9b12c2d819e1c1dd0a8d8753e5866c910a82e666
Lasse Herold Krarup <latze95@gmail.com>
2020-03-20 12:51:12 +0100

    v0.2 pages

--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,87 @@
 <template>
   <div>
-    <Hero class="is-fullheight is-primary" :arrow="true">
+    <Hero class="is-fullheight is-primary" :arrow="true" :background="images[1]">
       <template #first>
         <img src="~/assets/img/svg/LK_logo_white_text.svg" width="800px">
       </template>
 
       <template #second>
-        <h1 class="title">
+        <h1 class="title is-1 has-text-centered">
           {{ title }}
         </h1>
 
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. A impedit, voluptates provident ad eveniet rerum recusandae ducimus porro, quibusdam nesciunt nam quam. Aliquam itaque vel nobis quaerat blanditiis impedit quis dolore voluptas quas numquam iusto iure dolores, aspernatur, commodi explicabo. Dolorem sint impedit laudantium optio labore sit numquam corporis ducimus?
-        </p>
+        <h2 class="subtitle is-3 has-text-centered">
+          I'm a musician and a web designer
+        </h2>
+
+        <div class="columns content is-vcentered">
+          <div class="column has-text-centered">
+            <nuxt-link class="button is-large  is-danger is-outlined" to="/music">
+              Read about my music
+            </nuxt-link>
+          </div>
+          <div class="column has-text-centered">
+            <nuxt-link class="button is-large is-success is-outlined" to="/webdesign">
+              See my design work
+            </nuxt-link>
+          </div>
+        </div>
       </template>
     </Hero>
 
     <Section>
-      <Column>
-        <h1 class="title">
+      <Column delay="0.2s">
+        <h2 class="subtitle is-3">
           Drummer and session musician
-        </h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quos nobis at illum debitis! Quam quae aut unde, ad saepe at recusandae in eveniet quibusdam omnis possimus esse alias beatae, excepturi repudiandae facere soluta nulla accusantium? Recusandae deserunt eligendi, magnam sed mollitia voluptatibus alias velit reprehenderit perferendis modi voluptate facere?</p>
+        </h2>
+        <p class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum quos nobis at illum debitis! Quam quae aut unde, ad saepe at recusandae in eveniet quibusdam omnis possimus esse alias beatae, excepturi repudiandae facere soluta nulla accusantium? Recusandae deserunt eligendi, magnam sed mollitia voluptatibus alias velit reprehenderit perferendis modi voluptate facere?
+        </p>
 
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni perferendis beatae eaque tempora quas minus alias, nulla quis ratione doloremque sapiente sunt vitae iste dolorem inventore enim veniam? Quia itaque ullam eaque distinctio numquam, dolores expedita quasi, quidem officia, quod dolorem ratione sunt. Earum, odio similique. Asperiores recusandae quia neque!</p>
+        <p class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni perferendis beatae eaque tempora quas minus alias, nulla quis ratione doloremque sapiente sunt vitae iste dolorem inventore enim veniam? Quia itaque ullam eaque distinctio numquam, dolores expedita quasi, quidem officia, quod dolorem ratione sunt. Earum, odio similique. Asperiores recusandae quia neque!
+        </p>
       </Column>
-      <Column direction="right">
+      <Column direction="right" delay="0.5s">
         <img src="~/assets/img/close-up-photo-of-drum-set-995301.jpg" alt="Drummer">
       </Column>
     </Section>
 
-    <Section :class="'is-primary'">
-      <Column>
-        <img src="~/assets/img/beat-business-composition-concert-258668.jpg" alt="Drummer">
+    <Section class="is-primary" :background="images[0]" vcenter overlay-color="rgba(30,30,200,0.8)">
+      <Column direction="bottom" delay="0.8s" class="has-text-centered">
+        <nuxt-link class="button is-outlined is-large is-success" to="/webdesign">
+          See my work here
+        </nuxt-link>
       </Column>
-      <Column direction="right">
-        <h1 class="title">
-          Lessons, gigs or design
-        </h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, quas suscipit, nulla accusamus magnam a veniam earum ea, tempore cupiditate ducimus neque dicta voluptates accusantium consectetur. Tenetur, maiores. Rem dolorum alias, ut, aspernatur illum nemo, necessitatibus corporis harum dolorem molestias in ratione quis nulla sed. Aut, sequi. Atque, assumenda alias?</p>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor reiciendis ut provident velit tempora est eum expedita nihil ad molestiae laboriosam esse, totam ipsa veritatis qui nulla obcaecati corrupti, perferendis porro rerum necessitatibus blanditiis. Saepe fugit et officiis tempora deleniti quibusdam ullam cum sapiente! Laborum aut laudantium vitae excepturi. Mollitia.</p>
+      <Column direction="bottom">
+        <h2 class="subtitle is-3 has-text-white">
+          Gain online presence instantly
+        </h2>
+        <p class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi consequatur qui fugit cum minima rerum perferendis rem, consectetur aliquam repudiandae earum, natus porro repellat, aliquid corporis at tempora quo officiis soluta quaerat. Accusantium sit obcaecati quidem exercitationem nobis non quaerat.
+        </p>
+        <p class="content">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium itaque amet aliquam dolore provident deleniti, consectetur quos. Eius, sed. Mollitia reprehenderit praesentium sed sit hic expedita doloremque dolore dolores numquam, quidem possimus? Aut, ea animi! Quidem tenetur excepturi, molestias necessitatibus at aut repellendus expedita, quo vitae doloribus vel maiores quod!
+        </p>
+      </column>
+    </Section>
+
+    <Section>
+      <Column direction="top" class="has-text-centered">
+        <h2 class="subtitle is-3">
+          Hire me as a teacher
+        </h2>
+
+        <div class="content">
+          <ul>
+            <li v-for="(icon, idx) in musicIcons" :key="idx">
+              <span class="icon">
+                <i class="mdi" :class="'mdi-' + icon.name" />
+              </span>
+              {{ icon.description }}
+            </li>
+          </ul>
+        </div>
       </Column>
     </Section>
   </div>
@@ -60,7 +102,28 @@ export default Vue.extend({
   },
   data () {
     return ({
-      title: 'Drummer, musician, web designer'
+      title: 'Become seen. And heard.',
+      images: [
+        require('~/assets/img/apple-computer-decor-design-326502.jpg'),
+        require('~/assets/img/svg/frontpage-bg-graphic.svg')
+      ],
+      musicIcons: [
+        {
+          name: 'music-circle-outline',
+          description: 'Lessons with an emphasis on musicality, tailored to your individual needs'
+        },
+        {
+          name: 'music-clef-treble',
+          description: 'Rhythm training, drum technique and being the drummer people want to actually play with'
+        },
+        {
+          name: 'playlist-check',
+          description: "We'll create an effective and fun practice scheme to bolster your skills"
+        }
+      ],
+      designIcons: {
+
+      }
     })
   },
   head () {
@@ -74,5 +137,28 @@ export default Vue.extend({
 </script>
 
 <style lang="scss" scoped>
+$icon-margin: 1rem;
 
+ul {
+  display: flex;
+}
+
+li {
+  list-style: none;
+  text-decoration: none;
+  display: inline-flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: justify;
+  padding: 0 2rem;
+}
+
+.icon {
+  margin: $icon-margin 0;
+
+  i {
+    font-size: 3rem;
+    color: rgba(0,0,0,0.4);
+  }
+}
 </style>
---
+++ pages/music.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <Hero class="is-medium is-primary" :background="require('~/assets/img/svg/alt-bg-graphic.svg')">
+      <template #second>
+        <h1 class="title is-1">
+          {{ title }}
+        </h1>
+
+        <p class="content">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus minima sunt voluptatum necessitatibus dolore fugiat, obcaecati amet aliquam veniam delectus natus libero cupiditate, eaque modi aut sed nemo pariatur dignissimos.
+        </p>
+      </template>
+    </Hero>
+
+    <Section style="background-color: #241e20" class="has-text-white">
+      <Column class="center-content" direction="none">
+        <img :src="images[0]" alt="lightbulb" width="60%">
+      </Column>
+      <Column class="center-content">
+        <div class="content">
+          <h2 class="subtitle is-3 has-text-white">
+            Lessons
+          </h2>
+
+          <p class="content">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet nobis, amet rerum facere quam itaque quae quasi fugit est reprehenderit. Vel, aliquam, eligendi dignissimos molestiae, expedita pariatur nostrum unde deserunt doloremque quae laudantium ea minus sed quisquam ipsum fuga non fugit illum omnis quaerat delectus maxime dolore excepturi ab? Cum.
+          </p>
+        </div>
+      </Column>
+    </Section>
+
+    <Section class="is-primary">
+      <Column>
+        <h1 class="title">
+          Testy titleface
+        </h1>
+        <p class="content">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eveniet nobis, amet rerum facere quam itaque quae quasi fugit est reprehenderit. Vel, aliquam, eligendi dignissimos molestiae, expedita pariatur nostrum unde deserunt doloremque quae laudantium ea minus sed quisquam ipsum fuga non fugit illum omnis quaerat delectus maxime dolore excepturi ab? Cum.
+        </p>
+      </Column>
+      <Column />
+    </Section>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Hero from '~/components/Hero.vue'
+import Section from '~/components/Section.vue'
+import Column from '~/components/Column.vue'
+
+export default Vue.extend({
+
+  components: {
+    Hero,
+    Section,
+    Column
+  },
+  data () {
+    return ({
+      title: 'I teach, I gig, and I record',
+      images: [require('~/assets/img/close-up-photo-ofg-light-bulb-716398.jpg')]
+    })
+  },
+  head () {
+    const title:string = this.title
+    return ({
+      titleTemplate: 'Lasse Krarup - %s',
+      title
+    })
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.center-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+</style>
